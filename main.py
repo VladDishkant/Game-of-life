68a398bd91808b0ca8c27b0642dadaedb1c84140
GodSlayer3 <mm123.gg@bk.ru>
2021-04-08 11:26:59 +0300

    FINISHNA PRYAMA

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import random
 
+#CHECKING NEIGHBORS OF CELL
 def check_cell(field, row, column):
-    system = [[-1 , -1] , [-1 , 0] , [-1 , 1] , [0 , -1] , [0 , 1] , [1 , -1] , [1 , 0] , [1 , 1]]
+    system = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
     count = 0
     for i in system:
         x = row + i[0]
@@ -11,9 +13,9 @@ def check_cell(field, row, column):
         if y == -1:
             y = len(field[column]) - 1
 
-        if x == 5:
+        if x == len(field):
             x = 0
-        if y == 5:
+        if y == len(field[0]):
             y = 0
 
         if field[x][y] == 'x':
@@ -25,29 +27,49 @@ def check_cell(field, row, column):
         return count == 2 or count == 3
     return False
 
+
+#NEXT STATION OF FIELD
 def nextCircle(field):
-    field_new = ['',
-                 '',
-                 '',
-                 '',
-                 '']
+    field_new = []
 
     for row in range(len(field)):
+        field_new.append('')
         for column in range(len(field[row])):
             if check_cell(field, row, column):
                 field_new[row] += 'x'
             else:
                 field_new[row] += '.'
 
-    #print('\n'.join(field_new) + '\n')
+    # print('\n'.join(field_new) + '\n')
 
     return field_new
 
 
-field = ['.....',
-         '..x..',
-         'x....',
-         '..x..',
-         '.....']
+#CREATING RANDOM FIELD WITH WIDTH(ROWS) AND HEIGHT(COLUMNS)
+def randomField(rows, columns):
+    field = []
+    for row in range(rows):
+        field.append('')
+        for column in range(columns):
+            rand = random.random()
+            if rand>0.85:
+                symb = 'x'
+            else:
+                symb = '.'
+            field[row] += symb
+    return field
 
-nextCircle(field)
+
+#START POINT
+if __name__ == '__main__':
+    print("Количество циклов:")
+    cycle = int(input())
+    print("Размер сетки:")
+    rows = int(input())
+    columns = int(input())
+    field = randomField(rows,columns)
+    print('\n'.join(field) + '\n')
+    field_new = []
+    for i in range(cycle):
+        field_new = nextCircle(field)
+    print('\n'.join(field_new) + '\n')
--- tests.py
+++ tests.py
@@ -29,3 +29,27 @@ def test_nextStateExcpectedWrong():
                       '.....']
 
     assert main.nextCircle(field) != field_expected
+
+def test_randomField():
+    field = main.randomField(6,7)
+    assert len(field) == 6 and len(field[0]) == 7
+
+def test_randomFieldSameSize():
+    field = main.randomField(10,10)
+    assert len(field) == 10 and len(field[0]) == 10
+
+def test_CheckCell():
+    field = ['.....',
+             '..x..',
+             'x....',
+             '..x..',
+             '.....']
+    assert main.check_cell(field, 0, 0) == False
+
+def test_CheckCellTrue():
+    field = ['.....',
+             '..x..',
+             'x....',
+             '..x..',
+             '.....']
+    assert main.check_cell(field, 2, 1) == True
